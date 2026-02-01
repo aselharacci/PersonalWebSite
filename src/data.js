314b83fb594645b478c591d57570b2d341425a34
aselharacci <asel.haracci@gmail.com>
2026-02-02 00:45:17 +0300

    Fix invalid multiline strings in data.js

--- src/data.js
+++ src/data.js
@@ -78,10 +78,11 @@ const data = {
 			education2: "İstanbul Bilgi University",
 			education3: "Public Relations, 2019",
 			role1: "Preferred Role",
-			role2: "Frontend, UI",
+			role2: "Backend, UI",
 			aboutMe: "About Me",
-			aboutMeText1: "I'm an aspiring full-stack developer with a strong growth mindset, always eager to learn and challenge myself.After working as an account supervisor for 4 years, I realized it was time to follow my true passion: becoming a software developer.",
-			aboutMeText2: "The first step on this journey was completing a short Web Development course by Code First Girls, a UK-based organization that welcomes learners from all over the world. T o deepen my skills further, I completed a 6-months intensive full-stack development program at Workintech, where I focused on both front-end and back-end technologies, as well as core computer science fundamentals.",
+			aboutMeText1: `I'm an aspiring full-stack developer with a strong growth mindset, always eager to learn and challenge myself. After working as an account supervisor for 4 years, I realized it was time to follow my true passion: becoming a full-stack developer.`,
+			aboutMeText2: `I am currently enrolled in an intensive full-stack development bootcamp at Workintech, where I am honing my skills in JavaScript, React, Node.js, and more. My goal is to create efficient, scalable, and user-friendly web applications that make a positive impact.`,
+
 		},
 		projectsSection: {
 			title: "Projects",
